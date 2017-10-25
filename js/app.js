045c8d5e85a69bb9a5a38187a85632b2f71dcae4
ZoranPanev <panev.zoran.te@gmail.com>
2017-10-25 16:34:06 +0200

    create beta ver

--- js/app.js
+++ js/app.js
@@ -1,9 +1,15 @@
-$(function () {
-  function google() {
-    window.open("https://google.com");
-  }
+function nlbTerm() {
+  window.open("http://www.nlbtb.com.mk/");
+}
 
-  function bing() {
-    window.open("https://bing.com");
-  }
-});
+function nlbCreditInfo() {
+  window.open("http://www.nlb.mk/%D0%A4%D0%B8%D0%B7%D0%B8%D1%87%D0%BA%D0%B8_%D0%BB%D0%B8%D1%86%D0%B0/%D0%9F%D0%BE%D0%BD%D1%83%D0%B4%D0%B0/%D0%9A%D1%80%D0%B5%D0%B4%D0%B8%D1%82%D0%B8.aspx");
+}
+
+function nlbCredit() {
+  window.open("http://www.nlb.mk/%D0%A4%D0%B8%D0%B7%D0%B8%D1%87%D0%BA%D0%B8_%D0%BB%D0%B8%D1%86%D0%B0/%D0%9F%D0%BE%D0%BD%D1%83%D0%B4%D0%B0/%D0%9A%D1%80%D0%B5%D0%B4%D0%B8%D1%82%D0%B8/%D0%9F%D0%BE%D1%82%D1%80%D0%BE%D1%88%D1%83%D0%B2%D0%B0%D1%87%D0%BA%D0%B8_%D0%BA%D1%80%D0%B5%D0%B4%D0%B8%D1%82%D0%B8.aspx");
+}
+
+function nlbCourseList() {
+  window.open("http://www.nlb.mk/%D0%A4%D0%B8%D0%B7%D0%B8%D1%87%D0%BA%D0%B8_%D0%BB%D0%B8%D1%86%D0%B0/%D0%90%D0%BB%D0%B0%D1%82%D0%BA%D0%B8/%D0%9A%D1%83%D1%80%D1%81%D0%BD%D0%B0_%D0%BB%D0%B8%D1%81%D1%82%D0%B0.aspx");
+}
